b3bd9c641ed92dedd803c0171266f17199f397c8
Jesse Pinho <jesse@jessepinho.com>
2015-04-05 17:02:31 -0500

    Make more readable in TextEdit

--- encrypt.rb
+++ encrypt.rb
@@ -1,5 +1,6 @@
 # Take the alphabet, turn it into an array (a.k.a., a list), and then reverse
 # it.
+
 def reverse_alphabet
   ('a'..'z').to_a.reverse
 end
@@ -9,6 +10,7 @@ end
 # itself by calling "encrypt_character" on it. At the end, join the whole list
 # of newly encrypted characters back together so that it is text again (instead
 # of just an array of characters).
+
 def encrypt(text)
   text.each_char.map { |character|
     encrypt_character(character)
@@ -21,12 +23,13 @@ end
 # reverse_alphabet array. We add 1 to this number, though, because the computer
 # considers the first character in reverse_alphabet to be at position "0," not
 # "1," and the second character to be at position "1," not "2," and so on.
-#
+
 # Finally, at the end, we turn the number into a string (a.k.a., text) using
 # the "to_s" (short for "to string") method, and we right-justify using the
 # "rjust" method. We want it to be 2 characters long with a zero before it if
 # it's not already that long. This makes "1" turn into "01," but it leaves "15"
 # as-is.
+
 def encrypt_character(character)
   return character if character == ' '
   number = reverse_alphabet.find_index(character) + 1
@@ -40,6 +43,7 @@ end
 # two-character bits of text, then map each item in the array to the decrypted
 # version of itself by calling "decrypt_character" on it. At the end, join the
 # whole list back together so that it is text again.
+
 def decrypt(text)
   text.scan(/\d\d|\s/).map { |character|
     decrypt_character(character)
@@ -52,6 +56,7 @@ end
 # to subtract 1 from this number, though, for the same reason I described above:
 # computers consider the first character in reverse_alphabet to be at position
 # "0" instead of "1," and so on.
+
 def decrypt_character(character)
   return character if character == ' '
   reverse_alphabet[character.to_i - 1]
